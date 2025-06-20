24e6a01f47165dbc2a78146cd52ec88d4cc2306b
Swaroop <swaroop.atp@gmail.com>
2025-06-20 22:43:02 +0530

    added special variables

---
+++ roboshop.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0a225eb0390204f30"
+INSTANCES=("mongodb" "mysql" "rabbitmq" "redis" "catalogue" "user" "cart" "shipping" 
+"payment" "dispatch" "frontend")
+ZONE_ID="Z0885134QKOBUTU3UENH"
+DOMAIN_NAME="swaroopdevops.store"
+
+for instance in ${INSTANCES[@]}
+do
+
+INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id ami-09c813fb71547fc4f \
+    --instance-type t3.micro \
+    --security-group-ids sg-0a225eb0390204f30 \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
+    --output text)
+
+if [ $instance -ne "frontend"] 
+then
+IP=aws ec2 describe-instances \
+    --instance-ids $INSTANCE_ID \
+    --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+else
+    IP=aws ec2 describe-instances \
+    --instance-ids $INSTANCE_ID \
+    --query 'Reservations[0].Instances[0].PublicIpAddress' \
+fi
+    --output text
+echo "$instance IP Address is: $IP"
+done
