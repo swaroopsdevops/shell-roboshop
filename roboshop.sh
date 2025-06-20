b372b5c8b9e1a1eee3f7d7480bb41beeb75e588f
Swaroop <swaroop.atp@gmail.com>
2025-06-20 22:58:15 +0530

    added special variables

--- roboshop.sh
+++ roboshop.sh
@@ -9,26 +9,15 @@ DOMAIN_NAME="swaroopdevops.store"
 for instance in ${INSTANCES[@]}
 do
 
-    INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id ami-09c813fb71547fc4f \
-    --instance-type t3.micro \
-    --security-group-ids sg-0a225eb0390204f30 \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
-    --query 'Instances[0].InstanceId' \
-    --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0a225eb0390204f30 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=testSwarup}]' --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance -ne "frontend" ] 
     then
-        IP=aws ec2 describe-instances \
-        --instance-ids $INSTANCE_ID \
-        --query 'Reservations[0].Instances[0].PrivateIpAddress' \
-        --output text
+        IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text
     else
-        IP=aws ec2 describe-instances \
-        --instance-ids $INSTANCE_ID \
-        --query 'Reservations[0].Instances[0].PublicIpAddress' \
-        --output text
+        IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text
     fi
         
-    echo "$instance IP Address is: $IP"
+echo "$instance IP Address is: $IP"
+
 done
