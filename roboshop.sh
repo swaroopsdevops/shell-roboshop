4a96065052a112139952eb484ccdc357bf35ba53
Swaroop <swaroop.atp@gmail.com>
2025-06-20 23:09:18 +0530

    added special variables

--- roboshop.sh
+++ roboshop.sh
@@ -9,9 +9,9 @@ DOMAIN_NAME="swaroopdevops.store"
 for instance in ${INSTANCES[@]}
 do
 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0a225eb0390204f30 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=testSwarup}]' --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0a225eb0390204f30 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-    if [ $instance -ne "frontend" ] 
+    if [ $instance != "frontend" ] 
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
